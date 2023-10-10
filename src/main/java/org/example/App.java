88e4dbe9f4d62dfdf4b6eb99e3dd9cc059b12282
unknown <bickwang123@gmail.com>
2023-10-10 10:36:08 +0900

    1단계

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine();
+
+            if (command.equals("종료")) {
+                break;
+            }
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Scanner sc = new Scanner(System.in);
+        new App(sc).run();
+        sc.close();
     }
 }

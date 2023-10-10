737196ead7c0cd0fefaf49d3f6c5c7bcc4458408
sik2 <bickwang123@gmail.com>
2023-10-10 10:47:55 +0900

    2nd

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,10 +13,15 @@ public class App {
 
         while (true) {
             System.out.print("명령) ");
-            String command = sc.nextLine();
+            String command = sc.nextLine().trim();
 
             if (command.equals("종료")) {
                 break;
+            } else if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String author = sc.nextLine().trim();
             }
         }
     }
